eb9d1856b3f311af13fc5128659db17d0ca40f03
Muhammad Usman <usmannawaz53@gmail.com>
2020-05-26 07:35:57 +0500

    javaScript crash course added

---
+++ gettingStarted-with-the-Web/1-Dealing-with-files/web-projects/test-site/scripts/main.js
@@ -0,0 +1,11 @@
+
+// example 1:
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello javaScript';
+
+// example 2:
+const myChildHeading = document.querySelector('div h1');
+myChildHeading.textContent = 'HTML, CSS, javaScript';
+
+
+
---
+++ gettingStarted-with-the-Web/2-Language_basics_crash_course/script.js
@@ -0,0 +1,12 @@
+
+// Variables:
+
+let myVariable;
+myVariable = "Bop";
+console.log(myVariable);
+
+// you can do both these operations on the same line if you wish.
+// let myVariable = "Bop";
+
+myVariable = "JavaScript";
+console.log(myVariable);
