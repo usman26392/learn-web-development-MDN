882b0a6c74257bafe3e1560d00474feb17428d81
Muhammad Usman <usmannawaz53@gmail.com>
2021-04-25 07:21:27 +0500

    cv update

--- Muhammad-Usman-27-3-1992/assets/scripts/custom.js
+++ Muhammad-Usman-27-3-1992/assets/scripts/custom.js
@@ -2,10 +2,53 @@
 
 
 
-let crossBtn = document.querySelector(".left-cross-btn");
+let crossBtn = document.querySelector(".bi-x-circle");
+let toggleBtn = document.querySelector(".toggle-icon");
+let leftPanel = document.querySelector(".cv-left-column");
 
-let leftPanel = document.querySelector(".col-lg-3");
+// Toggle btn
+toggleBtn.addEventListener("click",()=> {
+    leftPanel.classList.toggle("show-left-column");
+    toggleBtn.firstElementChild.classList.toggle("bi-list");
+    toggleBtn.lastElementChild.classList.toggle("bi-x-circle");
+});
 
-crossBtn.addEventListener("click",()=> {
-    leftPanel.classList.add("hidden");
+
+window.setTimeout(() => {
+        leftPanel.classList.toggle("show-left-column");
+        toggleBtn.firstElementChild.classList.toggle("bi-list");
+        toggleBtn.lastElementChild.classList.toggle("bi-x-circle");
+    
+    
+}, 4000);
+
+
+
+// Tabs
+window.addEventListener("load", function() {
+	// store tabs variable
+	var myTabs = document.querySelectorAll("ul.nav-tabs > li");
+    function myTabClicks(tabClickEvent) {
+
+		for (var i = 0; i < myTabs.length; i++) {
+			myTabs[i].classList.remove("active");
+		}
+
+		var clickedTab = tabClickEvent.currentTarget;
+		clickedTab.classList.add("active");
+		tabClickEvent.preventDefault();
+		var myContentPanes = document.querySelectorAll(".tab-pane");
+
+		for (i = 0; i < myContentPanes.length; i++) {
+			myContentPanes[i].classList.remove("active");
+		}
+
+		var anchorReference = tabClickEvent.target;
+		var activePaneId = anchorReference.getAttribute("href");
+		var activePane = document.querySelector(activePaneId);
+		activePane.classList.add("active");
+	}
+	for (i = 0; i < myTabs.length; i++) {
+		myTabs[i].addEventListener("click", myTabClicks)
+	}
 });
